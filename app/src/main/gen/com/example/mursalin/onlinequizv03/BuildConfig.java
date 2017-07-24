483f11bc3ba671df41b60d0c75b6debbad5b49ca
anwar hossain <anwar hossain>
2017-07-24 12:17:38 +0600

    android

---
+++ app/src/main/gen/com/example/mursalin/onlinequizv03/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+package com.example.mursalin.onlinequizv03;
+
+/* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
+public final class BuildConfig {
+  public final static boolean DEBUG = Boolean.parseBoolean(null);
+}
